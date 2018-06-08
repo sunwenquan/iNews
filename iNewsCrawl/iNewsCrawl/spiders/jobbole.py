1805ab6f88043b6ce34d021e0f270ad8c8d1c396
gavinsun88 <sunwenquan@sina.cn>
2018-06-08 17:18:30 +0800

    Scrapy爬取前10条记录

--- iNewsCrawl/iNewsCrawl/spiders/jobbole.py
+++ iNewsCrawl/iNewsCrawl/spiders/jobbole.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 import scrapy
-
+from iNewsCrawl.items import InewscrawlItem
 
 class JobboleSpider(scrapy.Spider):
     name = 'jobbole'
@@ -8,4 +8,11 @@ class JobboleSpider(scrapy.Spider):
     start_urls = ['http://blog.jobbole.com/all-posts/']
 
     def parse(self, response):
-        pass
+        posts = response.css('div.post-meta')[:10]
+        for post in posts:
+            item = InewscrawlItem()
+            item['link']=post.css('a.archive-title::attr(href)').extract_first()
+            item['name'] =post.css('a.archive-title::text').extract_first()
+            print(item)
+            yield item
+
